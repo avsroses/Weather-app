0aab2a4d94dc51f1a0173c1e4fba17a0c126f60f
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-10-31 13:38:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,16 @@
 async function fetchData(url) {
     try {
         const response = await fetch(url);
+
+        // check response is ok
+        if(!response.ok) {
+            throw new Error("Response Status: ", response.status);
+        }
+
+        // obtain json
+        const json = await response.json();
+        console.log(json);
+
     } catch(error) {
         console.error(error);
     }
