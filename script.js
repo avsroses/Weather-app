9c97dbd24ea64310e7ea9a5418f2a1deeee6d38a
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-10-31 14:01:51 +0000

    update sliders based on json

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+const apiUrl = "http://localhost:5500/assets/data/test.json";
+
+// link sliders
+const windSlider = document.getElementById("wind-slider");
+const humiditySlider = document.getElementById("humidity-slider");
+const temperatureSlider = document.getElementById("temp-slider");
 
 /**
  * 
- * @param {string} url 
  */
-async function fetchData(url) {
+async function fetchData() {
     try {
-        const response = await fetch(url);
+        const response = await fetch(apiUrl);
 
         // check response is ok
         if(!response.ok) {
@@ -16,7 +21,31 @@ async function fetchData(url) {
         const json = await response.json();
         console.log(json);
 
+        // passes values into functions pulled from json
+        updateHumidity(json.current.humidity);
+        updateWindSpeed(json.current.wind_speed);
+        updateTemperature(json.current.temperature);
+
     } catch(error) {
         console.error(error);
     }
 }
+
+// updates humidity value on slider
+function updateHumidity(humidityValue) {
+    humiditySlider.value = humidityValue;
+}
+
+// updates wind speed value on slider
+function updateWindSpeed(windSpeedValue) {
+    windSlider.value = windSpeedValue;
+}
+
+// updates temperature value on slider
+function updateTemperature(temperatureValue) {
+    temperatureSlider.value = temperatureValue;
+}
+
+
+// fetch data every half second
+setInterval(fetchData, 500)
