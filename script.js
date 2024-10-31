349c2349b58f72e39c6ad573d43053ed3af6ca94
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-10-31 16:10:25 +0000

    adding new animations

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const apiUrl = "http://localhost:5500/assets/data/test.json";
+/*
+const apiUrl = "http://api.weatherstack.com/current?";
+const urlParams = {
+    query: "Norwich",
+    access_key: "be9f1bc4d9baecdeeae0e1f5a74ff5ed",
+};
+*/
 
 // link sliders and text
 const windSlider = document.getElementById("wind-slider");
@@ -14,7 +21,9 @@ const temperatureNum = document.getElementById("temperature");
 const leafAnimations = document.getElementsByClassName("leaf");
 
 // filter
-const humidityFilter = document.getElementById("humidity-filter")
+const humidityFilter = document.getElementById("humidity-filter");
+
+const dayOrNight = document.getElementById("background");
 
 /**
  * 
@@ -22,6 +31,7 @@ const humidityFilter = document.getElementById("humidity-filter")
 async function fetchData() {
     try {
         const response = await fetch(apiUrl);
+        //const response = await fetch(apiUrl + new URLSearchParams(urlParams));
 
         // check response is ok
         if(!response.ok) {
@@ -36,6 +46,9 @@ async function fetchData() {
         updateHumidity(json.current.humidity);
         updateWindSpeed(json.current.wind_speed);
         updateTemperature(json.current.temperature);
+
+        updateTimeOfDay(json.current.is_day);
+        
 
     } catch(error) {
         console.error(error);
@@ -69,6 +82,13 @@ function updateTemperature(temperatureValue) {
     temperatureNum.innerHTML = temperatureValue + "ºC";
 }
 
+function updateTimeOfDay(dayTime) {
+    if(dayTime === "no"){
+        dayOrNight.style.backgroundColor = "black";
+        dayOrNight.style.color = "white";
+    } 
+}
 
 // fetch data every half second
 setInterval(fetchData, 500)
+//fetchData()
